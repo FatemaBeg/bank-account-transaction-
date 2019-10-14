ea434ec4b427b1703dbe3d99803aa4084871e58b
FatemaBeg <fatema007beg@gmail.com>
2019-10-14 20:12:16 +0600

    account info

---
+++ AccountOperationAppPractice1/AccountOperationUi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AccountOperationAppPractice1
+{
+    public partial class AccountOperationUi : Form
+    {
+        Transaction transaction = new Transaction();
+        
+
+        public AccountOperationUi()
+        {
+            InitializeComponent();
+        }
+
+        private void DipositButton_Click(object sender, EventArgs e)
+        {
+         
+            transaction.account = Convert.ToInt32(amountTextBox.Text);
+            transaction.Deposit();
+            MessageBox.Show("Deposit complete");
+
+        }
+
+        private void WithdrawButton_Click(object sender, EventArgs e)
+        {
+            
+            transaction.account = Convert.ToInt32(amountTextBox.Text);
+            transaction.Withdraw();
+            MessageBox.Show("withdraw complete");
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show(transaction.Report());
+
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+           
+            transaction.name= customerNameTextBox.Text;
+            transaction.accountNo= accountNoTextBox.Text;
+            MessageBox.Show("account created");
+        }
+    }
+}
---
+++ AccountOperationAppPractice1/Transaction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationAppPractice1
+{
+  public  class Transaction
+    {
+        public int balance;
+        public int account;
+        public string name;
+        public string accountNo;
+
+      
+       public  int Withdraw()
+        {
+         
+            balance = balance - account;
+         
+            return balance;
+        }
+       public  int Deposit()
+        {
+           
+            balance = balance + account;
+           
+            return balance;
+        }
+        public string  Report()
+        {
+        
+            string report= ""+name+ ","+" your account number is  "+accountNo+"  and your balance is  " +balance.ToString();
+            return report;
+        }
+        
+    }
+}
